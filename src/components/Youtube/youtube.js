c6bee8c7c31a7a7685ee4a1298f71c0740a89b89
Jordan <jordan.porras1@gmail.com>
2019-10-14 02:12:55 -0300

    ultimo video subido

--- src/components/Youtube/youtube.js
+++ src/components/Youtube/youtube.js
@@ -5,7 +5,7 @@ class Youtube extends Component {
     render(){
         return(
             <div className = 'yt'>
-                <iframe className = 'ventana' src="https://www.youtube.com/embed/LqfJeokhLYc" frameBorder="0" allowFullScreen></iframe>
+                <iframe className = 'ventana' src="https://www.youtube.com/embed/videoseries?list=UUdvWRZTvKIo3BeRDWN7kfAg" frameBorder="0" allowFullScreen></iframe>
             </div>
         );
     }
