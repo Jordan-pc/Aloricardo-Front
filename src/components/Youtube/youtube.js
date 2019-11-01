b15653600f657d67d576e3576a8d86dd2d01692d
Jordan <jordan.porras1@gmail.com>
2019-11-01 19:15:50 -0300

    bugfix warning yt

--- src/components/Youtube/youtube.js
+++ src/components/Youtube/youtube.js
@@ -4,17 +4,8 @@ import './youtube.css';
 
 const Youtube = props => (
     <div className = 'yt'>
-        <iframe className = 'ventana' src="https://www.youtube.com/embed/videoseries?list=UUdvWRZTvKIo3BeRDWN7kfAg" frameBorder="0" allowFullScreen>Video</iframe>
+        <iframe title = 'yt_frame' className = 'ventana' src="https://www.youtube.com/embed/videoseries?list=UUdvWRZTvKIo3BeRDWN7kfAg" frameBorder="0" allowFullScreen>Video</iframe>
     </div>
 );
-// class Youtube extends Component {
-//     render(){
-//         return(
-//             <div className = 'yt'>
-//                 <iframe className = 'ventana' src="https://www.youtube.com/embed/videoseries?list=UUdvWRZTvKIo3BeRDWN7kfAg" frameBorder="0" allowFullScreen>Video</iframe>
-//             </div>
-//         );
-//     }
-// }
 
 export default Youtube;
